04f142f068929aa8dce330e65c886fe34db6d6d4
techbless <yunbin@hansung.ac.kr>
2020-11-23 12:01:35 +0900

    Polish design of SpeedPanel

--- src/SpeedPanel.java
+++ src/SpeedPanel.java
@@ -7,14 +7,15 @@ import javax.swing.JPanel;
 public class SpeedPanel extends JPanel {
 	public SpeedPanel() {
 		GridLayout grid = new GridLayout(2, 1);
+		grid.setVgap(-30);
 		setLayout(grid);
 		
 		
 		JLabel wpmLabel = new JLabel("WPM : 80", JLabel.CENTER);
 		JLabel accuracyLabel = new JLabel("ACCURACY : 99%", JLabel.CENTER);
 		
-		wpmLabel.setFont(new Font("Consolas", Font.PLAIN, 20));
-		accuracyLabel.setFont(new Font("Consolas", Font.PLAIN, 20));
+		wpmLabel.setFont(new Font("Consolas", Font.PLAIN, 18));
+		accuracyLabel.setFont(new Font("Consolas", Font.PLAIN, 18));
 		
 		add(wpmLabel);
 		add(accuracyLabel);
