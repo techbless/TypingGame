d5701fb708c75cfacad5e6aed97c713582682c6d
techbless <yunbin@hansung.ac.kr>
2020-11-23 15:51:56 +0900

    Add random word from WordSource on Game Object

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,28 +1,30 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.Timer;
 
 public class GamePanel extends JPanel {
 	
 	GameGroundPanel gameGroundPanel = new GameGroundPanel();
+	WordSource wordSource;
 	Vector<GameObject> gameObjects;
+	
 	
 	public GamePanel() {
 		gameObjects = new Vector<GameObject>(30);
+		wordSource = WordSource.getInstance();
 
 		setLayout(new BorderLayout());
-		new Thread(new GameObjectGenerator()).start();
 		add(gameGroundPanel, BorderLayout.CENTER);
 		add(new InputPanel(), BorderLayout.SOUTH);
 		
+		new Thread(new GameObjectGenerator()).start();
 	}
+	
 	
 	class GameGroundPanel extends JPanel {
 		public GameGroundPanel() {
@@ -31,36 +33,54 @@ public class GamePanel extends JPanel {
 		}
 	}
 	
+	
 	class InputPanel extends JPanel {
 		public InputPanel() {
 			setBackground(Color.GRAY);
-			add(new JTextField(40));			
+			JTextField inputField = new JTextField(40);
+			add(inputField);
+			
+			inputField.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					JTextField tField = (JTextField)(e.getSource());
+					String inWord = tField.getText();
+					
+					for(int i = 0; i < gameObjects.size(); i++) {
+						GameObject targetObj = gameObjects.get(i);
+						if(targetObj.getWord().equals(inWord)) {
+							gameGroundPanel.remove(targetObj);
+							gameGroundPanel.revalidate();
+							gameGroundPanel.repaint();
+							gameObjects.remove(i);
+							System.out.println("Answer : " + inWord);
+						}
+					}
+					
+					inputField.setText("");
+				}
+			});
 		}
 	}
 	
 	
-	
-	
 	class GameObjectGenerator implements Runnable {
-
 		@Override
 		public void run() {
 			int i = 0;
 			while(true) {
-				
-				Ghost newObject = new Ghost("dsfdsfsd", (int)(Math.random()*600), (int)(Math.random()*600));
+				Ghost newObject = new Ghost(wordSource.getWord(), 0, (int)(Math.random()*600));
 				gameObjects.add(newObject);
 				gameGroundPanel.add(newObject);
 				gameGroundPanel.repaint();
-				
+								
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			
 		}
-		
 	}
 }
